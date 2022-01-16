02da13919e93c99a389fbacc525e684a1cc6ae7f
Lode Kennes <lode@codechamp.be>
2022-01-16 20:05:10 +0100

    more fixes

--- get-attached-devices.js
+++ get-attached-devices.js
@@ -6,38 +6,43 @@ module.exports = function(RED) {
 
     const wrapper = require('axios-cookiejar-support').wrapper;
     const CookieJar = require('tough-cookie').CookieJar;
+    const httpCookieAgent = require('http-cookie-agent').HttpCookieAgent;
     const httpsCookieAgent = require('http-cookie-agent').HttpsCookieAgent;
 
     const jar = new CookieJar();
-    axios.defaults.httpsAgent = new httpsCookieAgent({
+    axios.defaults.httpAgent = new httpCookieAgent({
         jar,
         keepAlive: true,
         rejectUnauthorized: false, // disable CA checks
       });
+      axios.defaults.httpsAgent = new httpsCookieAgent({
+        jar,
+        keepAlive: true,
+        rejectUnauthorized: false, // disable CA checks
+      });
+      axios.defaults.headers.common = {
+        'User-Agent': 'HTTPie/2.3.0',
+        Accept: '*/*',
+      };
 
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', async function(msg) {
             const ip = this.credentials.username;
-            const instance =  wrapper(axios.create({
-                baseURL: `https://${ip}`,
-                withCredentials: true,
-                jar
-              }));
 
             const params = new url.URLSearchParams();
             params.append('submit_flag', 'sso_login');
             params.append('localPasswd', this.credentials.password);
             params.append('sso_login_type', '0');
 
-            const response = await instance.post(`/sso_login.cgi`, params)
+            const response = await axios.post(`https://${ip}/sso_login.cgi`, params)
             const token = response
             .headers["set-cookie"][0]
             .substring(10)
             .split(';')[0];
 
-            const devicesResponse = await instance.get('/refresh_dev.htm');
+            const devicesResponse = await axios.get(`https://${ip}/refresh_dev.htm`);
             const devices = devicesResponse.data;
 
             msg.payload = devices;
