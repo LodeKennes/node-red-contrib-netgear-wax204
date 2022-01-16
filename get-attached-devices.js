6514db9256fe817244cee17a43c867da860f29ea
Lode Kennes <lode@codechamp.be>
2022-01-16 19:18:22 +0100

    Added response

--- get-attached-devices.js
+++ get-attached-devices.js
@@ -3,10 +3,33 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
-            msg.ip = this.credentials.username;
-            msg.pw = this.credentials.password;
+            var details = {
+                'submit_flag': 'sso_login',
+                'localPasswd': this.credentials.password,
+                'sso_login_type': '0'
+            };
 
-            node.send(msg);
+            var formBody = [];
+            for (var property in details) {
+                var encodedKey = encodeURIComponent(property);
+                var encodedValue = encodeURIComponent(details[property]);
+                formBody.push(encodedKey + "=" + encodedValue);
+            }
+            formBody = formBody.join("&");
+
+            var ip = this.credentials.username;
+            fetch(`https://${ip}/sso_login.cgi`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                body: formBody
+              })
+            .then(r => {
+                msg.response = r;
+                node.send(msg);
+            });
+
         });
     }
     RED.nodes.registerType("get-attached-devices",LowerCaseNode, {
