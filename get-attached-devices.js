41e6aa3e4e7ce7a1b53a5f6c279b3b1437c94ccc
Lode Kennes <lode@codechamp.be>
2022-01-16 19:49:51 +0100

    device fetching should workd

--- get-attached-devices.js
+++ get-attached-devices.js
@@ -4,29 +4,37 @@ module.exports = function(RED) {
     const url = require('url');
     const https = require('https');
 
-    const instance = axios.create({
-        httpsAgent: new https.Agent({  
-          rejectUnauthorized: false
-        })
-      });
-
     function LowerCaseNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', async function(msg) {
+            const ip = this.credentials.username;
+            const instance = axios.create({
+                baseURL: `https://${ip}`,
+                httpsAgent: new https.Agent({  
+                  rejectUnauthorized: false
+                })
+              });
 
             const params = new url.URLSearchParams();
             params.append('submit_flag', 'sso_login');
             params.append('localPasswd', this.credentials.password);
             params.append('sso_login_type', '0');
 
-            const ip = this.credentials.username;
-            const response = await instance.post(`https://${ip}/sso_login.cgi`, params)
+            const response = await instance.post(`/sso_login.cgi`, params)
             const token = response
             .headers["set-cookie"][0]
-            .substring(10);
+            .substring(10)
+            .split(';')[0];
 
-            msg.token = token;
+            const devicesResponse = await instance.get('/refresh_dev.htm', {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            const devices = devicesResponse.data;
+
+            msg.payload = devices;
             node.send(msg);
         });
     }
